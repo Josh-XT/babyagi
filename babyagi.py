d2125b534503def0e405d4dece2819fc77c24fb0
Josh XT <josh@devxt.com>
2023-04-15 10:17:05 -0400

    Add FAISS, Update naming, Update local

--- babyagi.py
+++ babyagi.py
@@ -9,6 +9,7 @@ from typing import Dict, List
 import importlib
 import torch
 from transformers import LongformerTokenizer, LongformerModel
+from langchain.vectorstores import FAISS
 import openai
 import pinecone
 from dotenv import load_dotenv
@@ -16,48 +17,38 @@ from dotenv import load_dotenv
 # Load default environment variables (.env)
 load_dotenv()
 
+# Set your AI Model to use. You can use any of the models listed here:
+# Set to "ooba" for the Oobabooga Text Generation Web UI server
+AI_MODEL = os.getenv("AI_MODEL", "gpt-3.5-turbo")
+
 # Engine configuration
-
-USE_LOCAL_EMBEDDING = os.getenv("USE_LOCAL_EMBEDDINGS", "false")
-if USE_LOCAL_EMBEDDING.lower() == "true":
-    tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
-    model = LongformerModel.from_pretrained('allenai/longformer-base-4096')
-
-OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", "gpt-3.5-turbo")
-assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
-
-# If using OpenAI API, set the API key
-if OPENAI_API_MODEL.startswith("gpt-"):
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+# If we have an API key, use the OpenAI API
+if OPENAI_API_KEY:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
     assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
     openai.api_key = OPENAI_API_KEY
-
-if "gpt-4" in OPENAI_API_MODEL.lower():
-    print(
-        "\033[91m\033[1m"
-        + "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"
-        + "\033[0m\033[0m"
-    )
+    if "gpt-4" in AI_MODEL.lower():
+        print(
+            "\033[91m\033[1m"
+            + "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"
+            + "\033[0m\033[0m"
+        )
+else:
+    # Otherwise, use the local engine
+    print("\033[91m\033[1m" + "\n*****USING LOCAL ENGINE*****" + "\033[0m\033[0m")
+    tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
+    model = LongformerModel.from_pretrained('allenai/longformer-base-4096')
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
-assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable is missing from .env"
-
 PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "")
-assert (
-    PINECONE_ENVIRONMENT
-), "PINECONE_ENVIRONMENT environment variable is missing from .env"
-
-# Table config
-YOUR_TABLE_NAME = os.getenv("TABLE_NAME", "")
-assert YOUR_TABLE_NAME, "TABLE_NAME environment variable is missing from .env"
 
 # Goal configuation
 OBJECTIVE = os.getenv("OBJECTIVE", "")
 INITIAL_TASK = os.getenv("INITIAL_TASK", os.getenv("FIRST_TASK", ""))
 
 # Model configuration
-OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.0))
-
+AI_TEMPERATURE = float(os.getenv("AI_TEMPERATURE", 0.0))
 
 # Extensions support begin
 
@@ -80,7 +71,7 @@ if ENABLE_COMMAND_LINE_ARGS:
     if can_import("extensions.argparseext"):
         from extensions.argparseext import parse_arguments
 
-        OBJECTIVE, INITIAL_TASK, OPENAI_API_MODEL, DOTENV_EXTENSIONS = parse_arguments()
+        OBJECTIVE, INITIAL_TASK, AI_MODEL, DOTENV_EXTENSIONS = parse_arguments()
 
 # Load additional environment variables for enabled extensions
 if DOTENV_EXTENSIONS:
@@ -105,22 +96,29 @@ print(f"{OBJECTIVE}")
 
 print("\033[93m\033[1m" + "\nInitial task:" + "\033[0m\033[0m" + f" {INITIAL_TASK}")
 
-# Configure OpenAI and Pinecone
-
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-
-# Create Pinecone index
-table_name = YOUR_TABLE_NAME
-dimension = 1536
-metric = "cosine"
-pod_type = "p1"
-if table_name not in pinecone.list_indexes():
-    pinecone.create_index(
-        table_name, dimension=dimension, metric=metric, pod_type=pod_type
-    )
-
-# Connect to the index
-index = pinecone.Index(table_name)
+# Configure Vector DB
+if PINECONE_API_KEY:
+    # Table config
+    YOUR_TABLE_NAME = os.getenv("TABLE_NAME", "")
+    assert YOUR_TABLE_NAME, "TABLE_NAME environment variable is missing from .env"
+    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+    # Create Pinecone index
+    table_name = YOUR_TABLE_NAME
+    dimension = 1536
+    metric = "cosine"
+    pod_type = "p1"
+    if table_name not in pinecone.list_indexes():
+        pinecone.create_index(
+            table_name, dimension=dimension, metric=metric, pod_type=pod_type
+        )
+    # Connect to the index
+    index = pinecone.Index(table_name)
+else:
+    index = FAISS.from_texts(
+    texts=["_"],
+    embedding=model,
+    metadatas=[{"task": INITIAL_TASK}]
+)
 
 # Task list
 task_list = deque([])
@@ -135,7 +133,10 @@ def get_embedding(text, chunk_size=4096):
     
     # Split the text into smaller chunks
     chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
-    if USE_LOCAL_EMBEDDING.lower() == "true":
+    if OPENAI_API_KEY:
+        # Get embeddings for each chunk using OpenAI API
+        all_embeddings = [process_openai_embedding_chunk(chunk) for chunk in chunks]
+    else:
         all_embeddings = []
         for chunk in chunks:
             input_ids = tokenizer.encode(chunk, max_length=chunk_size, truncation=True, return_tensors='pt')
@@ -143,18 +144,14 @@ def get_embedding(text, chunk_size=4096):
                 outputs = model(input_ids)
                 embeddings = outputs.last_hidden_state[:, 0, :].numpy()
                 all_embeddings.append(embeddings[0])
-    else:
-        # Get embeddings for each chunk using OpenAI API
-        all_embeddings = [process_openai_embedding_chunk(chunk) for chunk in chunks]
     # Average the embeddings
     averaged_embedding = np.mean(all_embeddings, axis=0)
     return averaged_embedding
 
-
-def openai_call(
+def ai_call(
     prompt: str,
-    model: str = OPENAI_API_MODEL,
-    temperature: float = OPENAI_TEMPERATURE,
+    model: str = AI_MODEL,
+    temperature: float = AI_TEMPERATURE,
     max_tokens: int = 100,
 ):
     while True:
@@ -164,7 +161,7 @@ def openai_call(
                 cmd = ["llama/main", "-p", prompt]
                 result = subprocess.run(cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True)
                 return result.stdout.strip()
-            if model.startswith("ooba"):
+            if model == "ooba":
                 response = requests.post("http://localhost:7860/run/textgen", json={
                     "data": [
                         [
@@ -219,41 +216,57 @@ def openai_call(
                     stop=None,
                 )
                 return response.choices[0].message.content.strip()
-        except openai.error.RateLimitError:
-            print(
-                "The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again."
-            )
+        except:
+            print("The API rate limit has been exceeded. Waiting 10 seconds and trying again.")
             time.sleep(10)  # Wait 10 seconds and try again
         else:
             break
 
-
-def task_creation_agent(
-    objective: str, result: Dict, task_description: str, task_list: List[str]
-):
-    prompt = f"""
-    You are a task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective},
-    The last completed task has the result: {result}.
-    This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}.
-    Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks.
-    Return the tasks as an array."""
-    response = openai_call(prompt)
+def task_creation_agent(objective: str, result: Dict, task_description: str, task_list: List[str]):
+    if AI_MODEL == "ooba":
+            prompt = f"""
+You are a task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective},
+This is not a conversation, perform the task and return the result.
+The last completed task has the result: {result}.
+This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}.
+Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks.
+Return the tasks as an array.
+This is not a conversation, perform the task and return the results.
+Results:"""
+    else:
+        prompt = f"""
+You are a task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective},
+The last completed task has the result: {result}.
+This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}.
+Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks.
+Return the tasks as an array."""
+    response = ai_call(prompt)
     new_tasks = response.split("\n") if "\n" in response else [response]
     return [{"task_name": task_name} for task_name in new_tasks]
-
 
 def prioritization_agent(this_task_id: int):
     global task_list
     task_names = [t["task_name"] for t in task_list]
     next_task_id = int(this_task_id) + 1
-    prompt = f"""
-    You are a task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: {task_names}.
-    Consider the ultimate objective of your team:{OBJECTIVE}.
-    Do not remove any tasks. Return the result as a numbered list, like:
-    #. First task
-    #. Second task
-    Start the task list with number {next_task_id}."""
-    response = openai_call(prompt)
+    if AI_MODEL == "ooba":
+        prompt = f"""
+You are a task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: {task_names}.
+Consider the ultimate objective of your team:{OBJECTIVE}.
+Do not remove any tasks. Return the result as a numbered list, like:
+#. First task
+#. Second task
+Start the task list with number {next_task_id}.
+This is not a conversation, perform the task and return the results.
+Results:"""
+    else:
+        prompt = f"""
+You are a task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: {task_names}.
+Consider the ultimate objective of your team:{OBJECTIVE}.
+Do not remove any tasks. Return the result as a numbered list, like:
+#. First task
+#. Second task
+Start the task list with number {next_task_id}."""
+    response = ai_call(prompt)
     new_tasks = response.split("\n") if "\n" in response else [response]
     task_list = deque()
     for task_string in new_tasks:
@@ -262,7 +275,6 @@ def prioritization_agent(this_task_id: int):
             task_id = task_parts[0].strip()
             task_name = task_parts[1].strip()
             task_list.append({"task_id": task_id, "task_name": task_name})
-
 
 def execution_agent(objective: str, task: str) -> str:
     """
@@ -280,12 +292,19 @@ def execution_agent(objective: str, task: str) -> str:
     context = context_agent(query=objective, top_results_num=5)
     # print("\n*******RELEVANT CONTEXT******\n")
     # print(context)
-    prompt = f"""
-    You are an AI who performs one task based on the following objective: {objective}\n.
-    Take into account these previously completed tasks: {context}\n.
-    Your task: {task}\nResponse:"""
-    return openai_call(prompt, max_tokens=2000)
-
+    if AI_MODEL == "ooba":
+        prompt = f"""
+You are an AI who confidently performs one task based on the following objective: {objective}.
+Take into account these previously completed tasks: {context}.
+Your task to perform confidently: {task}.
+This is not a conversation, perform the task and return the results.
+Results:"""
+    else:
+        prompt = f"""
+You are an AI who performs one task based on the following objective: {objective}\n.
+Take into account these previously completed tasks: {context}\n.
+Your task: {task}\nResponse:"""
+    return ai_call(prompt, max_tokens=2000)
 
 def context_agent(query: str, top_results_num: int):
     """
@@ -300,12 +319,15 @@ def context_agent(query: str, top_results_num: int):
 
     """
     query_embedding = get_embedding(query)
-    results = index.query(query_embedding, top_k=top_results_num, include_metadata=True, namespace=OBJECTIVE)
-    # print("***** RESULTS *****")
-    # print(results)
-    sorted_results = sorted(results.matches, key=lambda x: x.score, reverse=True)
-    return [(str(item.metadata["task"])) for item in sorted_results]
-
+    if PINECONE_API_KEY:
+        results = index.query(query_embedding, top_k=top_results_num, include_metadata=True, namespace=OBJECTIVE)
+        sorted_results = sorted(results.matches, key=lambda x: x.score, reverse=True)
+        return [(str(item.metadata["task"])) for item in sorted_results]
+    else:
+        # Use FAISS
+        results = index.similarity_search_with_score(query, k=top_results_num)
+        sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
+        return [item[0].metadata["task"] for item in sorted_results]    
 
 # Add the first task
 first_task = {"task_id": 1, "task_name": INITIAL_TASK}
@@ -331,16 +353,23 @@ while True:
         print("\033[93m\033[1m" + "\n*****TASK RESULT*****\n" + "\033[0m\033[0m")
         print(result)
 
-        # Step 2: Enrich result and store in Pinecone
+        # Step 2: Enrich result and store in vector db
         enriched_result = {
             "data": result
         }  # This is where you should enrich the result if needed
-        result_id = f"result_{task['task_id']}"
-        vector = get_embedding(enriched_result["data"])  # get vector of the actual result extracted from the dictionary
-        index.upsert(
-            [(result_id, vector, {"task": task["task_name"], "result": result})],
-	    namespace=OBJECTIVE
-        )
+        if PINECONE_API_KEY:
+            result_id = f"result_{task['task_id']}"
+            vector = get_embedding(enriched_result["data"])  # get vector of the actual result extracted from the dictionary
+            index.upsert(
+                [(result_id, vector, {"task": task["task_name"], "result": result})],
+            namespace=OBJECTIVE
+            )
+        else:
+            # Use FAISS
+            result_id = f"result_{task['task_id']}"
+            vector = get_embedding(enriched_result["data"])
+            index.add_with_ids([vector], [result_id])
+            index.metadata.add(result_id, {"task": task["task_name"], "result": result})
 
         # Step 3: Create new tasks and reprioritize task list
         new_tasks = task_creation_agent(
