1518548d49330ccc435588ec94e9f957cae7357f
Josh XT <josh@devxt.com>
2023-04-15 20:12:35 -0400

    Update ENV, fix llama

--- provider/llama/__init__.py
+++ provider/llama/__init__.py
@@ -3,9 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-LLAMA_PATH = os.getenv("LLAMA_PATH")
-
 def instruct(model, prompt, temperature, max_tokens):
-    cmd = [f"{LLAMA_PATH}/main", "-p", prompt]
+    cmd = [f"llama/main", "-p", prompt]
     result = subprocess.run(cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True)
     return result.stdout.strip()
