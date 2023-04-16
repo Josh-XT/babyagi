39ae44f24ccd744e76e101a9e3075943422ebdb5
Josh XT <josh@devxt.com>
2023-04-16 12:14:29 -0400

    Moving tokenizer into function

--- embedding/longformer/__init__.py
+++ embedding/longformer/__init__.py
@@ -6,23 +6,22 @@ from transformers import LongformerTokenizer, LongformerModel
 load_dotenv()
 EMBEDDING_PROVIDER = os.getenv("AI_PROVIDER", "openai")
 
-if EMBEDDING_PROVIDER == "longformer":
-    tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
-    model = LongformerModel.from_pretrained('allenai/longformer-base-4096')
-    def custom_embedding(text):
-        return get_embedding(text)
-
 def get_embedding(text, chunk_size=4096):
-    text = text.replace("\n", " ")
-    # Split the text into smaller chunks
-    chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
-    all_embeddings = []
-    for chunk in chunks:
-        input_ids = tokenizer.encode(chunk, max_length=chunk_size, truncation=True, return_tensors='pt')
-        with torch.no_grad():
-            outputs = model(input_ids)
-            embeddings = outputs.last_hidden_state[:, 0, :].numpy()
-            all_embeddings.append(embeddings[0])
-    # Average the embeddings
-    averaged_embedding = np.mean(all_embeddings, axis=0)
-    return averaged_embedding
+    if EMBEDDING_PROVIDER == "longformer":
+        tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
+        model = LongformerModel.from_pretrained('allenai/longformer-base-4096')
+        def custom_embedding(text):
+            return get_embedding(text)
+        text = text.replace("\n", " ")
+        # Split the text into smaller chunks
+        chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+        all_embeddings = []
+        for chunk in chunks:
+            input_ids = tokenizer.encode(chunk, max_length=chunk_size, truncation=True, return_tensors='pt')
+            with torch.no_grad():
+                outputs = model(input_ids)
+                embeddings = outputs.last_hidden_state[:, 0, :].numpy()
+                all_embeddings.append(embeddings[0])
+        # Average the embeddings
+        averaged_embedding = np.mean(all_embeddings, axis=0)
+        return averaged_embedding
