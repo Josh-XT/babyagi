5e0a3d9cdce288350e7f1d8e3a5066162d79fd93
Josh XT <josh@devxt.com>
2023-04-17 14:32:15 -0400

    Updates

--- commands/create_new_command.py
+++ commands/create_new_command.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+from git import Repo
 from typing import List
 from Commands import Commands
 from Config import Config
@@ -10,6 +13,23 @@ class create_new_command(Commands):
         self.commands = {
             "Create a new command": self.create_command
         }
+
+    def git_pull(self) -> None:
+        repo = Repo('.')
+        origin = repo.remote(name='origin')
+        origin.pull()
+
+    def command_exists(self, file_name: str) -> bool:
+        return os.path.exists(f"commands/{file_name}.py")
+
+    def create_pull_request(self, file_name: str) -> None:
+        repo = Repo('.')
+        repo.git.add(f"commands/{file_name}.py")
+        repo.git.commit('-m', f"Add {file_name} command")
+        origin = repo.remote(name='origin')
+        origin.push()
+        # You need to set up GitHub CLI and authentication to create a pull request
+        subprocess.run(["gh", "pr", "create", "--title", f"Add {file_name} command", "--body", "Automatically created by Agent LLM."])
 
     def create_command(self, function_description: str) -> List[str]:
         args = [function_description]
@@ -31,16 +51,20 @@ class code_evaluation(Commands):
         prompt = f"You are now the following python function: ```# {description_string}\n{function_string}```\n\nOnly respond with your `return` value. Args: {args}"
         return AgentLLM().run(prompt, commands_enabled=False)
         """
-        description_string = """You write new commands for this framework. Ensure commands summaries are short and concice in self.commands.
-1. File name.
-2. Code for the new command that fits in this framework."""
+        description_string = "You write new commands for this framework. Ensure commands summaries are short and concice in self.commands. Do not explain, only provide code."
         prompt = f"{description_string}\n{function_string}```\n\nOnly respond with your `return` values. Args: {args}"
         response = AgentLLM().run(prompt, commands_enabled=False)
-        # Parse the response, 1 = file name, 2 = code
-        file_name = response.split("1. ")[1].split("\n")[0]
-        code = response.split("2. ")[1]
+        
+        # Git pull to update the local repository
+        self.git_pull()
+
+        file_name = response.split("class ")[1].split("(")[0]
         code = code.replace("```", "")
-        # Create a new file in the commands directory
-        with open(f"commands/{file_name}.py", "w") as f:
-            f.write(code)
-        return f"Created new command: {file_name}"
+
+        if not self.command_exists(file_name):
+            with open(f"commands/{file_name}.py", "w") as f:
+                f.write(code)
+            self.create_pull_request(file_name)
+            return f"Created new command: {file_name} and submitted a pull request."
+        else:
+            return f"Command {file_name} already exists. No changes were made."
